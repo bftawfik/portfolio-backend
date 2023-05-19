09b3c0f592a3251fcae16eb4d4d556f0d4261556
Khalil, Bishoy (contracted) <Bishoy.Khalil@contracted.pmi.com>
2023-05-19 12:35:25 +0200

    update firbase and express

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const firebase = require("firebase");
+const { initializeApp } = require("firebase/app");
+const { getDatabase, ref, onValue } = require("firebase/database");
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -12,18 +13,18 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 let data = {};
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getDatabase(firebaseApp);
+const dbRef = ref(db, 'pages/');
 
-firebase.initializeApp(firebaseConfig);
-let pages = firebase.database().ref("pages/");
-pages.on('value', function(snapshot) {
+onValue(dbRef, (snapshot) => {
   data = snapshot.val();
-  console.log(data);
-})
+});
 
-// console.log(process.env)
 app.get('/', (req, res) => res.send(data))
 
 app.listen(port, () => console.log('bftawfik-github-io API server started on: ' + port))
